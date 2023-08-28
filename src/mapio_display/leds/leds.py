78d6cdc62ee7adb1f624cb63310762f0592a865c
Pierrick <pierrickcurt@gmail.com>
2023-08-28 11:02:24 +0200

    feat: Add blink functions to led class

--- src/mapio_display/leds/leds.py
+++ src/mapio_display/leds/leds.py
@@ -14,6 +14,7 @@ class LED:
         """
         self.logger = logging.getLogger(__name__)
         color = color.upper()
+        self.number = number
 
         if number in [1, 2, 3] and color in ["G", "R", "B"]:
             self.led_path = "/sys/class/leds/LED" + str(number) + "_" + color
@@ -31,5 +32,34 @@ class LED:
         """Set a led to OFF"""
         try:
             os.system(f"echo 0 > {self.led_path}/brightness")  # nosec
+        except AssertionError:
+            self.logger.error("Unknown led")
+
+    def blink(self, period_ms: int, duty_cyle: int) -> None:
+        """Make a led blinking
+
+        Args:
+            period_ms (int): Blink period ms
+            duty_cyle (int): Duty cycle in percent
+        """
+        try:
+            os.system(f"echo timer > {self.led_path}/trigger")  # nosec
+            os.system(  # nosec
+                f"echo {round(period_ms * duty_cyle / 100)} > {self.led_path}/delay_on"  # nosec
+            )  # nosec
+            os.system(  # nosec
+                f"echo {round(period_ms * (100 - duty_cyle) / 100)} \
+                    > {self.led_path}/delay_off"  # nosec
+            )  # nosec
+
+        except AssertionError:
+            self.logger.error("Unknown led")
+
+    def reset(self, number: int) -> None:
+        """Reset all color for a specific led"""
+        try:
+            for color in ["R", "G", "B"]:
+                path = "/sys/class/leds/LED" + str(number) + "_" + color
+                os.system(f"echo 0 > {path}/brightness")  # nosec
         except AssertionError:
             self.logger.error("Unknown led")
